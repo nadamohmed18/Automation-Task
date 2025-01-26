caa53664ca1aed673c30144b4a06d2f38e2e04d6
nadamohmed18 <124162318+nadamohmed18@users.noreply.github.com>
2025-01-26 11:57:32 +0200

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,10 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    supportFile: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    specPattern: "Automation-Task/cypress/e2e/productCheckout/productCheckout.cy.js",
   },
 });
